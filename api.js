2c45ada40388d76c2f474d25a60aece56a92ec02
tranlinhptc <tranlinhptc@gmail.com>
2026-09-14 23:58:23 +0700

    Fix headers formatting in API call

--- api.js
+++ api.js
@@ -1,9 +1,12 @@
+// Cấu hình URL backend (GAS Web App)
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxqcQk3IX2zaDsjdBq76F3uMpVjCU5_nVDjTQ_vgYZUnpS_zsQewSwQnQraqhCvHU36/exec';
+
 async function callAPI(action, payload = {}) {
   const body = JSON.stringify({ action, ...payload });
   const res = await fetch(GAS_URL, {
     method: 'POST',
     body: body,
-    headers: { 'Content-Type': 'text/plain;charset=utf-8' } // tránh CORS preflight
+    headers: { 'Content-Type': 'text/plain;charset=utf-8' }
   });
   return await res.json();
 }
