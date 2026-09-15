a281b5079a846737b076a2dbc6e712a259c8d9b1
tranlinhptc <tranlinhptc@gmail.com>
2026-09-15 19:00:54 +0700

    Update GAS_URL to new endpoint

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // ⚠️ DÁN URL GAS VÀO ĐÂY
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbxqcQk3IX2zaDsjdBq76F3uMpVjCU5_nVDjTQ_vgYZUnpS_zsQewSwQnQraqhCvHU36/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbyhnEPOy8x9rMJZ75z1ajlWMeaCZAz7lF3z4vWxPg5vXd0OznSBKvSEmZqcuU0lLBZO/exec';
